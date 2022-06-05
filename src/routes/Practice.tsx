3270c6454ac56aa2e81dc77e4304c4aab34a3e36
tombloor <5075314+tombloor@users.noreply.github.com>
2022-06-04 22:34:30 -0500

    cleaned up demo

--- src/routes/Practice.tsx
+++ src/routes/Practice.tsx
@@ -4,31 +4,54 @@ import { Decoder } from "../components/Decoder";
 const data = {
     'quote': 'Welcome to the Daily Quota demo!',
     'cipher': 'abcdefghijklmnopqrstuvwxyz' // Users attempt (will be state eventually)
+    , 'encrypted': ''
 }
 
+const applyCipher = (text: string, cipher: string) => {
+    let tmp = '';
+
+    text.split('').forEach((char, index) => {
+        if (char.toLowerCase() != char.toUpperCase()) {
+            // This is an alpha character
+            let cipherIndex = char.toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0);
+            if (char.toLowerCase() == char) {
+                tmp += cipher[cipherIndex];
+            } else {
+                tmp += cipher[cipherIndex].toUpperCase();
+            }
+        } else {
+            tmp += char;
+        }
+    });
+
+    return tmp;
+}
+
+// This is just for the demo
+const demoCipher = data.cipher.split('').sort(() => { return 0.5 - Math.random() }).join('');
+data.encrypted = applyCipher(data.quote, demoCipher);
+//
 
 export default function Practice(props: any) {
 
-    const [decodedQuote, setDecodedQuote] = useState(data.quote);
+    const [decodedQuote, setDecodedQuote] = useState(data.encrypted);
+    const [cipher, setCipher] = useState(data.cipher);
 
     const handleCipherChange = (newCipher: string) => {
-        let tempQuote = data.quote;
-
-        for (let i = 0; i < newCipher.length; i++) {
-            let char = String.fromCharCode('a'.charCodeAt(0) + i);
-            tempQuote = tempQuote.replaceAll(char.toLowerCase(), newCipher[i].toLowerCase());
-            tempQuote = tempQuote.replaceAll(char.toUpperCase(), newCipher[i].toUpperCase());
-        }
-
-        setDecodedQuote(tempQuote);
+        setCipher(newCipher);
+        setDecodedQuote(applyCipher(data.encrypted, newCipher));
     }
 
     return (
     <>
         <h2>Practice Page</h2>
+        <p>
+            Right now this is just a demo of how the letter replacement will work. Try changing the letters to make
+            the text match.
+        </p>
         <p>{data.quote}</p>
         <p>{decodedQuote}</p>
-        <Decoder cipher={data.cipher} onChange={handleCipherChange} />
+        <Decoder cipher={cipher} onChange={handleCipherChange} />
     </>
     )
 }
