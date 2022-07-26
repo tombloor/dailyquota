d5c93144737062f57b1d4220b799122d1149768b
tombloor <5075314+tombloor@users.noreply.github.com>
2022-07-25 23:30:23 -0500

    renamed functions file for clarity

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,1 +1,1 @@
-export * from './challenge';
+export * from './challengeFunctions';
--- functions/src/services/data.ts
+++ functions/src/services/data.ts
@@ -1,8 +1,9 @@
 import { Challenge, Quote } from '../models'
 
+// Checkout the docs on firestore converters for a nice way to map interfaces 
 
 export const createQuote = (quote: Quote): Quote => {
-    let id = 'TODO: SAVE IN FIRESTORE';
+    let id = 'TODO: SAVE IN FIRESTORE'; // This should be a hash of the normalised quote text and the source (quotable::this is the quote)
 
     return {
         ...quote,
@@ -13,7 +14,7 @@ export const createQuote = (quote: Quote): Quote => {
 
 
 export const createChallenge = (quote: Quote, cipher: string, encoded: string): Challenge => {
-    let id = 'TODO: SAVE IN FIRESTORE';
+    let id = 'TODO: SAVE IN FIRESTORE'; // For daily challenges this could be the date? For practices just a guid (these will be separate collections)
 
     return {
         id,
