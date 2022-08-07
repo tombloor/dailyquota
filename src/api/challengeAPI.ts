4ad30bbbd112fe1b527b8f184275fc892d040d06
tombloor <5075314+tombloor@users.noreply.github.com>
2022-08-06 23:09:47 -0500

    call check solution from client app

--- src/api/challengeAPI.ts
+++ src/api/challengeAPI.ts
@@ -2,3 +2,5 @@ import { functions } from '../services';
 import { httpsCallable } from 'firebase/functions';
 
 export const createChallenge = httpsCallable<null, {challenge_id: string, encoded: string, author: string}>(functions, "requestChallenge");
+
+export const checkSolution = httpsCallable<{challenge_id: string, decoded_text: string}, any>(functions, "checkSolution");
